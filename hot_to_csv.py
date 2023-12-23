6170589706303b039218d0ab1d5fa8d3ced58bc9
HCoder1949 <61670998+HCoder1949@users.noreply.github.com>
2023-12-23 20:50:16 +0800

    Update hot_to_csv.py

--- hot_to_csv.py
+++ hot_to_csv.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     api_data = get_api_data(api_url)
     
     if api_data:
-        for key in list(api_data['element_list'].keys())[1:-1]:
+        for key in list(api_data['map_list'].keys())[1:-1]:
             print(key)
             indexs = [item["rank"] for item in api_data['result']['%s' % key]]
 
